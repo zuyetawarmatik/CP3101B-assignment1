09bd4feaf610cb432bd15c704b9806efa0feff4e
zuyetawarmatik <francesc.zuyet@gmail.com>
2014-02-12 17:59:13 +0800

    Setup

--- application/Router.class.php
+++ application/Router.class.php
@@ -25,5 +25,38 @@ class Router {
         $this->path = $path;
     }
 
+    /* Router load the controller */
+    public function loader()
+    {
+        /*** check the route ***/
+        $this->getController();
+        
+        /*** if the file is not there diaf ***/
+        if (is_readable($this->file) == false)
+        {
+            $this->file = $this->path.'/error404.php';
+            $this->controller = 'error404';
+        }
+    
+        /*** include the controller ***/
+        include $this->file;
+        
+        /*** a new controller class instance ***/
+        $class = $this->controller . 'Controller';
+        $controller = new $class($this->registry);
+        
+        /*** check if the action is callable ***/
+        if (is_callable(array($controller, $this->action)) == false)
+        {
+            $action = 'index';
+        }
+        else
+        {
+            $action = $this->action;
+        }
+        /*** run the action ***/
+        $controller->$action();
+    }
+
 }
 ?>
