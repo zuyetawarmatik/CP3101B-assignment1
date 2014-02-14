62c6762c38acd960c8dce84acbfb89c956e51a11
zuyetawarmatik <francesc.zuyet@gmail.com>
2014-02-14 23:12:45 +0800

    Index page

--- view/index.view.php
+++ view/index.view.php
@@ -6,6 +6,7 @@
 		<meta http-equiv="Pragma" content="no-cache">
 		<meta http-equiv="Expires" content="0">
 		<link rel="stylesheet" type="text/css" href="./view/asset/font/Source_Sans_Pro/stylesheet.css">
+		<link rel="stylesheet" type="text/css" href="./view/asset/font/Simple_Line_Icons/stylesheet.css">
 		<link rel="stylesheet" type="text/css" href="./view/asset/css/reset.css">
 		<link rel="stylesheet" type="text/css" href="./view/asset/css/shared.css">
 		<link rel="stylesheet" type="text/css" href="./view/asset/css/index.view.css">
@@ -13,8 +14,9 @@
 	</head>
 	<body>
 		<section id="top-section">
-			<div id="top-section-content">
+			<div id="top-section-content">			
 				<div id="logo">
+					Tasaka Managara&#8482;
 				</div>
 				<nav id="nav-bar">
 					<ul>
@@ -31,11 +33,39 @@
 			</div>
 		</section>
 		<section id="intro-section">
-			<h1>We are Tasaka Managana</h1>
+			<h1>We are Tasaka Managara</h1>
 			<p>Simply the best task manager in the world.</p>
 		</section>
 		<section id="main-section">
 			<div id="main-section-content">
+				<ul id="feature-list">
+					<li>
+						<a>
+							<h1><i>&#57376;</i></h1>
+							<h2>Fast Task Management</h2>
+							<p>We maintain a high quality product and aim for consumer productivity.<br>You can create and manage tasks everywhere, everytime in just 1 click.</p>
+						</a>
+					</li>
+					<li>
+						<a>
+							<h1><i>&#57350;</i></h1>
+							<h2>A+ for Usability</h2>
+							<p>We will never confuse you.<br>No need for a tutorial kickstart, you can jump straight at creating your schedule.</p>
+						</a>
+					</li>
+					<li>
+						<a>
+							<h1><i>&#57386;</i></h1>
+							<h2>Hey You, It's Free!</h2>
+							<p>Free forever. As this is our freebie to the loved world.</p>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</section>
+		<section id="footer-section">
+			<div id="footer-section-content">
+				<p>Created with love from Team Tasaka Managara. Copyright &#169; 2014.</p>
 			</div>
 		</section>
 	</body>
