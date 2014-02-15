1d918f1b7f4b5d1ff6e969ddea0032160e0f9bfd
zuyetawarmatik <francesc.zuyet@gmail.com>
2014-02-15 18:04:02 +0800

    Login css fix

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 	$db_config = [
-		'db_type' => 'pgsql',
+		'db_type' => 'mysql',
 		'db_host' => 'localhost',
-		'db_port' => 5432,
+		'db_port' => 3306,
 		'db_name' => 'cp3101b-1',
-		'db_username' => 'postgres',
-		'db_password' => 'root'
+		'db_username' => 'root',
+		'db_password' => ''
 	];
 ?>
