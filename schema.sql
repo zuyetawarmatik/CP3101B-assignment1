a1037a566e76961ffe71e6a8bfd4891c45a9e28c
Navie Huynh <quahuynh@gmail.com>
2014-02-15 19:41:57 +0800

    change to username in schema

--- schema.sql
+++ schema.sql
@@ -1,23 +1,24 @@
 DROP SCHEMA IF EXISTS "public" cascade;
 CREATE SCHEMA "public";
 
+CREATE SEQUENCE user_id_seq;
+CREATE SEQUENCE task_id_seq;
+
 CREATE TABLE "users" (
-  id integer PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  -- email VARCHAR(200),
-  password VARCHAR(50)
+	id INTEGER NOT NULL default nextval('user_id_seq') PRIMARY KEY,
+	username VARCHAR(50) NOT NULL,
+	email VARCHAR(200),
+	password VARCHAR(50)
 );
 
 CREATE TABLE "tasks" (
-  id integer PRIMARY KEY,
-  user_id integer 
-    constraint FK_task_user
-    REFERENCES users(id) ,
-  description VARCHAR(200)
+	id INTEGER NOT NULL default nextval('user_id_seq') PRIMARY KEY,
+	user_id integer 
+	constraint FK_task_user
+	REFERENCES users(id) ,
+	description VARCHAR(200)
 );
 
 
-INSERT INTO users VALUES(1,'name','pwd');
-INSERT INTO tasks VALUES(1,1,'task desc');
 SELECT * FROM users;
 SELECT * FROM tasks;
