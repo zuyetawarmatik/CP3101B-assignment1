491264c4f4e3946f73d7e441b1e95c670bc42b86
zuyetawarmatik <francesc.zuyet@gmail.com>
2014-02-16 23:55:09 +0800

    Fix 404 page

--- controller/TaskController.class.php
+++ controller/TaskController.class.php
@@ -1,54 +1,59 @@
 <?php
 
 class TaskController extends BaseController {
-	//TODO: page not found 404 e.g. /task/dsdasd
+	// TODO: page not found 404 e.g. /task/dsdasd
 	public function index() {
-		//if (isset($_SESSION['login'])) {
-		$this->registry->template->highlight = "tasks";
-		$this->registry->template->username = $_SESSION['login']['username'];
-		$tasks = Task::getTasksByUserId($_SESSION['login']['id']);
-		$this->registry->template->tasks = $tasks;
-
-		$this->registry->template->show('tasks');
-
-		//} else {
-			//return (new Error404Controller($this->registry))->index();	
-		//}
-	}
-	public function edit() {
-		$user_id = $_SESSION['login']['id'];
-		if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-			$task_id = $_GET['task_id'];
-			if (is_numeric($task_id)) {
-				$task = Task::getTaskByIdAndOwnerId($task_id,$user_id);
-				$this->registry->template->task_id = $task_id;
-				$this->registry->template->name = $task->name;
-				$this->registry->template->description = $task->description;
-				$this->registry->template->blocks = $task->blocks;
-				$this->registry->template->current_block = $task->current_block;
-
-				$this->registry->template->highlight = "tasks";
-				$this->registry->template->show('task_edit');
-			}
-
-		}elseif ($_SERVER['REQUEST_METHOD'] == 'POST'){
-			$task_id = $_POST['task_id'];
-			$name = $_POST["name"];
-			$desc = $_POST["description"];
-			$blocks = $_POST["blocks"];
-
-			$task = Task::getTaskByIdAndOwnerId($task_id,$user_id);
-			if($task!=null){
-				$task->name = $name;
-				$task->description = $desc;
-				$task->blocks = $blocks;
-				$task->current_block = min($task->current_block,$blocks);
-				if($task->save()){
-					header("Location: " . __BASE_URL . "task/");
-				};
-			}
+		if (isset($_SESSION['login'])) {
+			$this->registry->template->highlight = "tasks";
+			$this->registry->template->username = $_SESSION['login']['username'];
+			$tasks = Task::getTasksByUserId($_SESSION['login']['id']);
+			$this->registry->template->tasks = $tasks;
+	
+			$this->registry->template->show('tasks');
+		} else {
+			return (new Error404Controller($this->registry))->index();	
 		}
 	}
+	
+	public function edit() {
+		if (isset($_SESSION['login'])) {
+			$user_id = $_SESSION['login']['id'];
+			if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+				$task_id = $_GET['task_id'];
+				if (is_numeric($task_id)) {
+					$task = Task::getTaskByIdAndOwnerId($task_id,$user_id);
+					$this->registry->template->task_id = $task_id;
+					$this->registry->template->name = $task->name;
+					$this->registry->template->description = $task->description;
+					$this->registry->template->blocks = $task->blocks;
+					$this->registry->template->current_block = $task->current_block;
+	
+					$this->registry->template->highlight = "tasks";
+					$this->registry->template->show('task_edit');
+				}
+	
+			} else if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+				$task_id = $_POST['task_id'];
+				$name = $_POST["name"];
+				$desc = $_POST["description"];
+				$blocks = $_POST["blocks"];
+	
+				$task = Task::getTaskByIdAndOwnerId($task_id,$user_id);
+				if($task!=null){
+					$task->name = $name;
+					$task->description = $desc;
+					$task->blocks = $blocks;
+					$task->current_block = min($task->current_block,$blocks);
+					if($task->save()){
+						header("Location: " . __BASE_URL . "task/");
+					};
+				}
+			}
+		} else {
+			return (new Error404Controller($this->registry))->index();
+		}
+	}
+	
 	public function revertblock(){
 		if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 			$user_id = $_SESSION['login']['id'];
@@ -57,11 +62,12 @@ class TaskController extends BaseController {
 			$task->current_block = max(0, $task->current_block-1);
 			if ($task->save()){
 				header("Location: " . __BASE_URL . "task/");
-			}else{
+			} else{
 				echo "error saving task current_block";
 			}
 		}
 	}
+	
 	public function nextblock(){
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$task_id = $_POST['task_id'];
@@ -70,17 +76,18 @@ class TaskController extends BaseController {
 
 			if ($task->save()){
 				header("Location: " . __BASE_URL . "task/");
-			}else{
+			} else{
 				echo "error saving task current_block";
 			}
 		}
 	}
+	
 	public function create() {
-		//if (isset($_SESSION['login'])) {
+		if (isset($_SESSION['login'])) {
 			if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 				$this->registry->template->highlight = "tasks";
 				$this->registry->template->show('task_create');
-			}else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+			} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				$name = $_POST["name"];
 				$desc = $_POST["description"];
 				$blocks = $_POST["blocks"];
@@ -99,9 +106,9 @@ class TaskController extends BaseController {
 					echo 'error';
 				};
 			}
-		//} else {
-			//return (new Error404Controller($this->registry))->index();	
-		//}
+		} else {
+			return (new Error404Controller($this->registry))->index();	
+		}
 	}
 }
 
--- view/error404.view.php
+++ view/error404.view.php
@@ -8,7 +8,7 @@
 		<?php include('include/top_section.inc'); ?>
 		<section id="main-section">
 			<div id="main-section-content">
-				<h1>Sorry, the page does not exist.</h1>
+				<h1>Sorry, the page does not exist<br>or you haven't logged in yet.</h1>
 			</div>
 		</section>
 		<?php include('include/footer_section.inc'); ?>
