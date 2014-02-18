20df49d093f2415cb6da71dd70ff9dff40af32a4
Navie Huynh <quahuynh@gmail.com>
2014-02-18 14:28:33 +0800

    XSS filter in template class

--- application/Template.class.php
+++ application/Template.class.php
@@ -9,10 +9,47 @@ class Template {
 	function __construct($registry) {
 		$this->registry = $registry;
 	}
+	private function escape($value){
+		if(gettype($value)=="array"){
+			$escaped=array();
+			foreach ($value as $el) {
+				array_push($escaped,$this->escape($el));
+			}
+			return $escaped;
+		} else if (gettype($value)=="string"){
+			return htmlentities($value);
+		}elseif (gettype($value)=="object"){
+			if ($value instanceof Task){
+				$value->name = htmlentities($value->name);
+				$value->description = htmlentities($value->description);
+				return $value;
+			}else{
+				throw	"Passing unknown object type to view is not allowed.";
+			}
+		}else {
+			$this->vars[$index] = ($value);
+		}
+	}
 
 	public function __set($index, $value)
 	{
-		$this->vars[$index] = $value;
+
+		$this->vars[$index] = $this->escape($value);
+
+		if (gettype($value)=="string"){
+			$this->vars[$index] = htmlentities($value);
+		}elseif (gettype($value)=="object"){
+			if ($value instanceof Task){
+				$value->name = htmlentities($value->name);
+				$value->description = htmlentities($value->description);
+				$this->vars[$index] = ($value);
+			}else{
+				throw	"Passing unknown object type to view is not allowed.";
+			}
+		}else{
+			$this->vars[$index] = ($value);
+		}
+
 	}
 
 	function show($name) {
