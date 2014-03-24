4543d80b8f12dd060a0fe341dfa97e5b022a4b59
Navie Huynh <quahuynh@gmail.com>
2014-03-24 13:57:45 +0800

    fixed minor bug

--- tasks/add.php
+++ tasks/add.php
@@ -4,11 +4,9 @@ include '../include.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	status(201);
 	print json_encode(array(
-			array(
 				"id" => 1,
 				"description" => "buy milk",
 				"num_blocks" => 30
-			)
 	));
 }
 ?>
