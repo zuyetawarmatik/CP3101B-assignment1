b7aa2af5fa8fed4302a0322a85ea005a3222b443
Navie Huynh <quahuynh@gmail.com>
2014-03-29 02:33:34 +0800

    task revert

---
+++ service/tasks/revert.php
@@ -0,0 +1,35 @@
+<?php
+include '../include.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	$user_id = $_SESSION['login']['id'];
+	$task_id = $_GET['id'];
+	if (!is_numeric($task_id)){
+		status(400);
+		print json_encode(array(
+			"error" => array("task id must be numberic")
+		));
+		return;
+	};
+	$task = Task::getTaskByIdAndOwnerId($task_id,$user_id);
+	if ($task==null){
+		status(404);
+		return;
+	}
+	$task->current_block = max(0, $task->current_block-1);
+	if ($task->save()){
+		status(200);
+		print json_encode(array(
+			"id" => $task->id,
+			"description" => $task->description,
+			"created_time" => $task->created_time,
+			"name" => $task->name,
+			"description" => $task->description,
+			"num_blocks" => $task->blocks,
+			"current_block" => $task->current_block
+		));
+	} else{
+		status(400);
+	}
+}
+?>
