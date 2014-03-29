96064a2592bb27622e0ee7121f598365c819fc1f
Navie Huynh <quahuynh@gmail.com>
2014-03-29 17:22:18 +0800

    added missing file

---
+++ service/task/details.php
@@ -0,0 +1,30 @@
+<?php
+include '../include.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	if ($_SESSION['login']!=null){
+		status(200);
+		$task_id = $_POST['id'];
+		$task = Task::getTaskByIdAndOwnerId($task_id,$_SESSION['login']['id']);
+		if ($task==null){
+			status(404);
+			print "{}";
+			return;
+		}
+
+		print json_encode(array(
+				"id" => $task->id,
+				"name" => $task->name,
+				"description" => $task->description,
+				"num_blocks" => $task->blocks,
+				"current_block" => $task->current_block,
+				"created_time" => $task->created_time
+			
+		));
+	}else{
+		status(401);
+		print "{}";
+	}
+
+}
+?>
